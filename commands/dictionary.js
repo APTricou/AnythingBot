7978770b2e3c84bd42be8643a77e3618df6b42ad
Antanas Tricou <APTricou@gmail.com>
2020-03-10 13:55:04 -0500

    adjusting formatting in dictionary response

--- commands/dictionary.js
+++ commands/dictionary.js
@@ -5,11 +5,11 @@ module.exports = (word, msg) => {
         console.log(res.data)
         const message = res.data.list
         .slice(0,3) // limit because discord message limit is 2000 characters
-        .map(entry=>`${entry.word.trim()} - 
+        .map(entry=>`\`\`\`${entry.word.trim()} - 
                     ${entry.definition.replace(/[\[\]\n]/g,'').trim()}\n
                     ${entry.example.replace(/[\[\]]/g,'').trim()}\n
                     ${entry.author.trim()}\n`).join("")
-        msg.reply(message)
+        msg.reply(message)\`\`\``
     })
     .catch(error=>{
         console.error(error)
