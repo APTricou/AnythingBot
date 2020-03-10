624ac986165471bd9423ac07acec102b496c5616
Antanas Tricou <APTricou@gmail.com>
2020-03-10 14:06:04 -0500

    even more formatting

--- commands/dictionary.js
+++ commands/dictionary.js
@@ -3,11 +3,10 @@ module.exports = (word, msg) => {
     axios.get(`http://api.urbandictionary.com/v0/define?term=${word}`)
     .then(res=>{
         console.log(res.data)
-        const message = res.data.list
+        const message = "\n Here are the top three results from Urban Dictionary" +
+        res.data.list
         .slice(0,3) // limit because discord message limit is 2000 characters
-        .map(entry=>`\`\`\`${entry.word.trim()} - ${entry.definition.replace(/[\[\]\n]/g,'').trim()}\n
-        ${entry.example.replace(/[\[\]]/g,'').trim()}\n
-            ${entry.author.trim()}\`\`\``).join("")
+        .map(entry=>`\`\`\`${entry.word.trim()} - ${entry.definition.replace(/[\[\]\n]/g,'').trim()}\n${entry.example.replace(/[\[\]]/g,'').trim()}\n${entry.author.trim()}\`\`\``).join("")
         msg.reply(message)
     })
     .catch(error=>{
